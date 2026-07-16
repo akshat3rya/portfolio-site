e7232c3e58eba58f5fad590fc5b670041727a3a9
akshat3rya <akshat3rya@gmail.com>
2026-07-16 21:27:22 +0530

    added routing

--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,8 @@ import "./styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
-);
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
 );
